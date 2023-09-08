343f1c092f7f4fb7c0d8afc8d2e810e2ba41a00f
Ihor Osypov <ihor.osypov@geeksforless.com>
2023-09-08 18:33:23 +0300

    fix error

--- components/Header.tsx
+++ components/Header.tsx
@@ -20,8 +20,12 @@ const Header = () => {
     <header>
       <nav className="border-b-2 bg-slate-50 h-10 shadow-sm shadow-slate-300">
         <div className="container mx-auto flex items-center h-full">
-          {pages.map((p) => (
-            <Link className="mr-2 font-bold text-slate-600" href={p.href}>
+          {pages.map((p, index) => (
+            <Link
+              key={index}
+              className="mr-2 font-bold text-slate-600"
+              href={p.href}
+            >
               {p.name}
             </Link>
           ))}
